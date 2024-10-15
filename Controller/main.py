85661bba1c86e4f9d4d8b65137e685083a12fae2
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-10-15 02:51:20 +0100

    edit Controller/main.py
    adding class wsManager,

--- Controller/main.py
+++ Controller/main.py
@@ -6,6 +6,27 @@ from fastapi import FastAPI, WebSocket
 app = FastAPI()
 
 
+class wsManager:
+    def __init__ (self):
+        self.Rooms : list[WebSocket] = []
+    
+    async def connect (self, room: WebSocket):
+        await room.accept()
+        self.Rooms.append (room)
+
+    async def send_message (self, room: WebSocket, clientName: str, message: str):
+        await room.send_text(f'{clientName}: {message}.')
+
+    async def broadcast (self, room: WebSocket, clientName: str, message: str):
+        for vcRoom in self.Rooms:
+            if vcRoom != room:
+                await self.send_message (vcRoom, clientName, message)
+
+    async def disconnect (self, room: WebSocket, clientName: str):
+        self.Rooms.remove (room)
+        await self.broadcast(room, clientName, f'{clientName} has left the room !')
+
+
 
 @app.websocket('/room')
 async def wsroom (room: WebSocket, clientName: str):
