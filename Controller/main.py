5b8203f5eeeb7ace5fb55c34afe4d251c91c6954
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-10-15 02:55:40 +0100

    editing Controller/main.py
    editing websocket route (/room),
    editing the logic of the chat so it matches to the wsManager

--- Controller/main.py
+++ Controller/main.py
@@ -1,5 +1,5 @@
 
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 
 
@@ -28,9 +28,15 @@ class wsManager:
 
 
 
+manager = wsManager()
+
 @app.websocket('/room')
 async def wsroom (room: WebSocket, clientName: str):
-    await room.accept()
-    while True:
-        data = await room.receive_text()
-        await room.send_text(data)
+    await manager.connect(room)
+
+    try:
+        while True:
+            message = await room.receive_text ()
+            await manager.broadcast(room, clientName, message)
+    except WebSocketDisconnect:
+        await manager.disconnect(room, clientName)
