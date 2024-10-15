e4ebba0929bde6ee352d4593a6f18d89ec45a48c
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-10-15 19:48:28 +0100

    editing Controller/main.py,   adding Controller/wsockets/manager.py
    replacing wsManager into a seperate module,

--- Controller/main.py
+++ Controller/main.py
@@ -1,39 +1,9 @@
 
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-
+from fastapi import FastAPI
+from .wsockets.manager import WebSocket, wsManager, WebSocketDisconnect
 
 
 app = FastAPI()
-
-
-class wsManager:
-    def __init__ (self):
-        self.Rooms : list[WebSocket] = []
-    
-    async def connect (self, room: WebSocket):
-        await room.accept()
-        self.Rooms.append (room)
-
-    async def send_message (self, room: WebSocket, clientName: str, message: str):
-        await room.send_text (f'{clientName}: {message}')
-
-    async def send_alerts (self, room: WebSocket, message: str):
-        await room.send_text (f'{message}')
-
-    async def broadcast (self, room: WebSocket, clientName: str, message: str):
-        for vcRoom in self.Rooms:
-            if vcRoom != room:
-                await self.send_message (vcRoom, clientName, message)
-
-    async def broadcast_alerts (self, room: WebSocket, message: str):
-        for vcRoom in self.Rooms:
-            if vcRoom != room:
-                await self.send_alerts(vcRoom, message)
-
-    async def disconnect (self, room: WebSocket, clientName: str):
-        self.Rooms.remove (room)
-        await self.broadcast_alerts(room, f'{clientName} has left the room !')
-
 
 
 manager = wsManager()
---
+++ Controller/wsockets/manager.py
@@ -0,0 +1,33 @@
+
+from fastapi import WebSocket, WebSocketDisconnect
+
+class wsManager:
+    def __init__ (self):
+        self.Rooms : list[WebSocket] = []
+    
+    async def connect (self, room: WebSocket):
+        await room.accept()
+        self.Rooms.append (room)
+
+    async def send_message (self, room: WebSocket, clientName: str, message: str):
+        await room.send_text (f'{clientName}: {message}')
+
+    async def send_alerts (self, room: WebSocket, message: str):
+        await room.send_text (f'{message}')
+
+    async def broadcast (self, room: WebSocket, clientName: str, message: str):
+        for vcRoom in self.Rooms:
+            if vcRoom != room:
+                await self.send_message (vcRoom, clientName, message)
+
+    async def broadcast_alerts (self, room: WebSocket, message: str):
+        for vcRoom in self.Rooms:
+            if vcRoom != room:
+                await self.send_alerts(vcRoom, message)
+
+    async def disconnect (self, room: WebSocket, clientName: str):
+        self.Rooms.remove (room)
+        await self.broadcast_alerts(room, f'{clientName} has left the room !')
+        if room.client_state == room.client_state.CONNECTED:
+            await websocket.close()
+
