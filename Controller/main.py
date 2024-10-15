3abc985d2f19cc9ddbda2d5189357310475c9968
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-10-15 03:16:01 +0100

    editing Controller/main.py
    editing wsManager class,
    adding func send_alert (), adding func broadcast_alerts (),
    editing func disconnect(),

--- Controller/main.py
+++ Controller/main.py
@@ -15,16 +15,24 @@ class wsManager:
         self.Rooms.append (room)
 
     async def send_message (self, room: WebSocket, clientName: str, message: str):
-        await room.send_text(f'{clientName}: {message}.')
+        await room.send_text (f'{clientName}: {message}')
+
+    async def send_alerts (self, room: WebSocket, message: str):
+        await room.send_text (f'{message}')
 
     async def broadcast (self, room: WebSocket, clientName: str, message: str):
         for vcRoom in self.Rooms:
             if vcRoom != room:
                 await self.send_message (vcRoom, clientName, message)
 
+    async def broadcast_alerts (self, room: WebSocket, message: str):
+        for vcRoom in self.Rooms:
+            if vcRoom != room:
+                await self.send_alerts(vcRoom, message)
+
     async def disconnect (self, room: WebSocket, clientName: str):
         self.Rooms.remove (room)
-        await self.broadcast(room, clientName, f'{clientName} has left the room !')
+        await self.broadcast_alerts(room, f'{clientName} has left the room !')
 
 
 
